14e49770dbd20afedd408220566804ee5d472212
Ryan Braid <rbraid@mines.edu>
2020-03-18 09:55:46 -0600

    Apparently we won't run sfresco on transfer data, so i should delete it rather than comment it

--- runTransfer.bash
+++ runTransfer.bash
@@ -19,45 +19,6 @@ echo -e "Beinning to convert Fresco output to ROOT output"
 tput sgr0
 python $UTILDIR/slimgrace2root.py fort.16 transfer_before.root
 
-#
-# tput setaf 2
-# echo -e "Beinning to convert angular distribution to the search file for sfresco"
-# tput sgr0
-# tput setaf 1
-# echo -e "NOT IMPLEMENTED, SHORT CIRCUIT PAST SFRESCO"
-# tput sgr0
-# python $UTILDIR/angdist2fresco.py $ANGDIR/angOutReal.root transfer.search
-
-
-# tput setaf 2
-# echo -e "Beinning to run sfresco"
-# tput sgr0
-# sfresco < sfrescoCommands.txt
-#
-# elif [ $MODE == "PARAM" ]
-# then
-# sfresco <<EOF
-# elastic_$1_$2_$3_$4_$5_$6_$7.search
-# fix 1
-#
-# min
-# migrand
-# end
-#
-# plot elastic_$1_$2_$3_$4_$5_$6_$7.plot
-# exit
-# EOF
-# fi
-# echo
-# echo
-#
-# tput setaf 2
-# echo -e "Beinning to convert sfresco output to ROOT"
-# tput sgr0
-#
-# UTILDIR/slimgrace2root.py elastic.plot elastic_after.root
-
-
 # tput setaf 2
 # echo -e "Beinning to make a blurred version of the sfresco fit"
 # tput sgr0
