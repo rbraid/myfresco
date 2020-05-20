28f02ba98b213af2b5974318b0ae8194346f5d32
Ryan Braid <rbraid@mines.edu>
2020-05-20 15:03:52 -0600

    updated color assignments to be unique to each state

--- SpectroscopicFactor/DrawSpectroscopics.py
+++ SpectroscopicFactor/DrawSpectroscopics.py
@@ -18,13 +18,17 @@ if args.Mode != "oneMinus" and args.Mode != "twoPlus" and args.Mode != "twoMinus
   quit()
 
 dataPointer = -1
+gColor = ROOT.kBlack
 
 if args.Mode == "oneMinus":
   dataPointer = 2
+  gColor = ROOT.kRed
 elif args.Mode == "twoMinus":
   dataPointer = 3
+  gColor = ROOT.kBlue
 elif args.Mode == "twoPlus":
   dataPointer = 4
+  gColor = ROOT.kGreen
 
 sfrescoF = TFile.Open("{}.root".format(args.Mode),"read")
 
@@ -49,22 +53,23 @@ Dummy.SetAxisRange(.0000001,10000,"Y")
 
 leg = TLegend(0.65,.65,.9,.9)
 
-frescoH.SetMarkerColor(colors[0])
-frescoH.SetLineColor(colors[0])
+frescoH.SetMarkerColor(ROOT.kBlack)
+frescoH.SetLineColor(ROOT.kBlack)
 frescoH.SetFillColor(ROOT.kWhite)
 leg.AddEntry(frescoH,"FRESCO Raw Output")
 frescoH.Draw("sameL")
 
-dataHAfter.SetMarkerColor(colors[1])
-dataHAfter.SetLineColor(colors[1])
+dataHAfter.SetMarkerColor(ROOT.TColor.GetColorDark(gColor))
+dataHAfter.SetLineColor(ROOT.TColor.GetColorDark(gColor))
 dataHAfter.SetFillColor(ROOT.kWhite)
 dataHAfter.SetMarkerStyle(20)
 leg.AddEntry(dataHAfter,"Data")
 dataHAfter.Draw("sameP")
 
-sfrescoH.SetMarkerColor(colors[2])
-sfrescoH.SetLineColor(colors[2])
+sfrescoH.SetMarkerColor(gColor)
+sfrescoH.SetLineColor(gColor)
 sfrescoH.SetFillColor(ROOT.kWhite)
+sfrescoH.SetLineWidth(2)
 leg.AddEntry(sfrescoH,"SFRESCO Output")
 sfrescoH.Draw("sameL")
 
