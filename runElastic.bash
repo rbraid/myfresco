719a163ae11824f40ada71dc6b5700406ec36134
Ryan Braid <rbraid@mines.edu>
2020-06-26 13:47:19 -0600

    Dropped support for optical only, which was a misguided coulomb scattering from when I didn't know any better.

--- runElastic.bash
+++ runElastic.bash
@@ -15,12 +15,8 @@ elif [ $@ == "full" ] || [ $@ == "Full" ] || [ $@ == "FULL" ]
 then
   echo "Full Mode Active"
   MODE="FULL"
-elif [ $@ == "optical" ] || [ $@ == "Optical" ] || [ $@ == "OPTICAL" ]
-then
-  echo "Optical Only Active"
-  MODE="OPTICAL"
 else
-  echo "Error, Expect 'full' or 'optical' as an argument"
+  echo "Error, Expect 'full' as an argument"
   return 0
 fi
 
@@ -35,9 +31,6 @@ then
   if [ $MODE == "FULL" ]
   then
     echo -e "Beinning to run Elastic"
-  elif [ $MODE == "OPTICAL" ]
-  then
-    echo -e "Beinning to run Elastic Optical Only"
   fi
   tput sgr0
   bash clean.bash
@@ -48,9 +41,6 @@ then
   if [ $MODE == "FULL" ]
   then
   fresco < elastic.in > elastic.out
-  elif [ $MODE == "OPTICAL" ]
-  then
-  fresco < elastic_opticalOnly.in > elastic_opticalOnly.out
   fi
   echo
 
@@ -60,9 +50,6 @@ then
   if [ $MODE == "FULL" ]
   then
   python $UTILDIR/slimgrace2root.py fort.16 elastic_before.root
-  elif [ $MODE == "OPTICAL" ]
-  then
-  python $UTILDIR/slimgrace2root.py fort.16 elastic_opticalOnly_before.root
   fi
 
   # tput setaf 2
@@ -71,9 +58,6 @@ then
   # if [ $MODE == "FULL" ]
   # then
   # python $UTILDIR/calcNorm.py elastic_before.root ~/nuclear/mine/rb/angulardistribution/angOutReal.root elastic_norm.root
-  # elif [ $MODE == "OPTICAL" ]
-  # then
-  # python $UTILDIR/calcNorm.py elastic_opticalOnly_before.root ~/nuclear/mine/rb/angulardistribution/angOutReal.root elastic_opticalOnly_norm.root
   # fi
 fi
 
@@ -92,9 +76,6 @@ plot elastic_rRuth.plot
 exit
 EOF
 
-elif [ $MODE == "OPTICAL" ]
-then
-python $UTILDIR/angdist2fresco.py $ANGDIR/angOutReal.root elastic_opticalOnly.search
 elif [ $MODE == "PARAM" ]
 then
 python $UTILDIR/angdist2fresco.py $ANGDIR/angOutReal.root elastic_$1_$2_$3_$4_$5_$6_$7.search $1 $2 $3 $4 $5 $6 $7
@@ -159,9 +140,6 @@ plot elastic_rRuth_after.plot
 exit
 EOF
 
-elif [ $MODE == "OPTICAL" ]
-then
-sfresco < sfrescoCommands_opticalOnly.txt
 elif [ $MODE == "PARAM" ]
 then
 sfresco <<EOF
@@ -191,9 +169,6 @@ python $UTILDIR/slimgrace2root.py elastic.plot elastic_after.root
 python $UTILDIR/slimgrace2root.py elastic_rRuth.plot elastic_rRuth_before.root
 python $UTILDIR/slimgrace2root.py elastic_rRuth_after.plot elastic_rRuth_after.root
 
-elif [ $MODE == "OPTICAL" ]
-then
-python $UTILDIR/slimgrace2root.py elastic_opticalOnly.plot elastic_opticalOnly_after.root
 elif [ $MODE == "PARAM" ]
 then
 python $UTILDIR/slimgrace2root.py elastic_$1_$2_$3_$4_$5_$6_$7.plot elastic_after_$1_$2_$3_$4_$5_$6_$7.root
@@ -230,12 +205,6 @@ echo -e "Generating nice png"
 tput sgr0
 python $UTILDIR/plotter.py full True
 python $UTILDIR/plotter.py full False
-elif [ $MODE == "OPTICAL" ]
-then
-tput setaf 2
-echo -e "Generating nice png"
-tput sgr0
-python $UTILDIR/plotter.py optical
 elif [ $MODE == "PARAM" ]
 then
 python $UTILDIR/plotter.py param $1_$2_$3_$4_$5_$6_$7
