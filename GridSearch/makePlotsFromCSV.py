32376e4741629c6cb83d4d981b32f6b6cbb71aca
Ryan Braid <rbraid@mines.edu>
2020-07-15 15:34:23 -0600

    Support for finer grain grid search

--- GridSearch/makePlotsFromCSV.py
+++ GridSearch/makePlotsFromCSV.py
@@ -18,13 +18,13 @@ inFrame.drop(inFrame[inFrame.aW != .9].index, inplace=True)
 
 print "After Selecting variables, inFrame holds {} entries".format(len(inFrame))
 
-V_W_plot_blur = TH2D("V_W_plot_blur","Comparing Blurred Chi Square of V vs W;V;W", 60,25,85, 21,15,120)
+V_W_plot_blur = TH2D("V_W_plot_blur","Comparing Blurred Chi Square of V vs W;V;W", 25,45,70, 25,30,55)
 [V_W_plot_blur.Fill(x,y,z) for x, y, z in zip(inFrame["V"], inFrame["W"], inFrame["BlurChiSquare"])]
 
-V_W_plot_prod = TH2D("V_W_plot_prod","Comparing Product of Blurred Chi Square and Regular Chi Square of V vs W;V;W", 60,25,85, 21,15,120)
+V_W_plot_prod = TH2D("V_W_plot_prod","Comparing Product of Blurred Chi Square and Regular Chi Square of V vs W;V;W", 25,45,70, 25,30,55)
 [V_W_plot_prod.Fill(x,y,z) for x, y, z in zip(inFrame["V"], inFrame["W"], inFrame["BlurChiSquare"] * inFrame["Chi Square"])]
 
-V_W_plot_init = TH2D("V_W_plot_init","Comparing Regular Chi Square of V vs W;V;W", 60,25,85, 21,15,120)
+V_W_plot_init = TH2D("V_W_plot_init","Comparing Regular Chi Square of V vs W;V;W", 25,45,70, 25,30,55)
 [V_W_plot_init.Fill(x,y,z) for x, y, z in zip(inFrame["V"], inFrame["W"], inFrame["Chi Square"])]
 # for row in inFrame:
 #     print row
